90f27a0e815526cd7fd00fc197e76b506ff52dfd
ejwww <51363519+ejwww@users.noreply.github.com>
2024-01-14 12:23:41 +0800

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,63 @@
+import math
+'''
+T = 4
+
+
+sum =  0
+for t in range(0,T):
+
+    a = math.sqrt(T - t + 1)
+    sum += a
+    n = int(32*a/sum)
+    print(n)
+
+T = 6
+
+total_sum = 0
+n_list = []
+for t in range(0, T):
+    a = math.sqrt(T - t )
+    total_sum += a
+    n = int(32 * a / total_sum)
+    n_list.append(n)
+
+# 计算缩放比例
+scale = 32 / sum(n_list)
+
+# 缩放每个n值
+n_list = [int(n * scale) for n in n_list]
+
+# 调整最后一个n值以确保总和为32
+n_list[-1] += 32 - sum(n_list)
+
+# 打印每个n值和总和
+print("n values:", n_list)
+print("Sum of n values:", sum(n_list))
+'''
+T = 4
+
+total_sum = 0
+n_list = []
+for t in range(0, T):
+    a = math.sqrt(T - t + 1)
+    total_sum += a
+    n = int(128 * a / total_sum)
+    n_list.append(n)
+
+# 计算缩放比例
+scale = 128 / sum(n_list)
+
+# 缩放每个n值
+n_list = [int(n * scale) for n in n_list]
+
+# 调整最后一个n值以确保总和为32
+n_list[-1] += 128 - sum(n_list)
+
+# 判断最后一个n值是否为1，如果是，则后续的n值都设为1
+if n_list[-1] == 1:
+    n_list[-1:] = [1] * (T - len(n_list) + 1)
+
+# 打印每个n值和总和
+print("n values:", n_list)
+print("Sum of n values:", sum(n_list))
+
