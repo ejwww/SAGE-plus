525d1adf639d7e4662fdbe99f5169089702d78cb
ejwww <51363519+ejwww@users.noreply.github.com>
2024-05-30 12:37:54 +0800

    Update GraphSAGE++DAMC.py

--- GraphSAGE++DAMC.py
+++ GraphSAGE++DAMC.py
@@ -1,157 +1,86 @@
 import torch
 import torch.nn.functional as F
-from matplotlib import pyplot as plt
-from sklearn.manifold import TSNE
-from sklearn.metrics import f1_score
-from torch_geometric.nn import MessagePassing
-from torch_geometric.utils import add_self_loops, degree
-from torch_geometric.datasets import Planetoid, Amazon
-from torch_geometric.nn import SAGEConv
 import torch_geometric.nn as pyg_nn
 import torch.nn as nn
-from torch_geometric.utils import train_test_split_edges
-from torch_geometric.datasets import PPI
-from torch_geometric.loader import NeighborSampler
-from torch_geometric.utils import negative_sampling
-import time
+from torch_geometric.datasets import Planetoid
+
 
 class GraphSAGEPlusPlusDAMC(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
         super().__init__()
         self.num_layers = num_layers
-
         self.convs_mean = torch.nn.ModuleList()
         self.convs_max = torch.nn.ModuleList()
 
+        # 添加一个初始的全连接层来调整特征维度
+        self.initial_lin = nn.Linear(in_channels, hidden_channels)
+
         for _ in range(num_layers):
-            self.convs_mean.append(SAGEConv(in_channels if _ == 0 else hidden_channels, hidden_channels, aggr='mean'))
-            self.convs_max.append(SAGEConv(in_channels if _ == 0 else hidden_channels, hidden_channels, aggr='max'))
+            conv_mean = pyg_nn.SAGEConv(hidden_channels, hidden_channels, aggr='mean')
+            conv_max = pyg_nn.SAGEConv(hidden_channels, hidden_channels, aggr='max')
+            self.convs_mean.append(conv_mean)
+            self.convs_max.append(conv_max)
 
         self.post_mp = nn.Linear(num_layers * 2 * hidden_channels, out_channels)
 
-    def reset_parameters(self):
-        for conv in self.convs_mean:
-            conv.reset_parameters()
-        for conv in self.convs_max:
-            conv.reset_parameters()
-        self.post_mp.reset_parameters()
+    def forward(self, x, edge_index):
+        # 通过初始全连接层调整输入特征的维度
+        x = self.initial_lin(x)
+        x = F.relu(x)  # 通常在全连接层后应用非线性激活函数
 
-    def forward(self, x, adjs):
         all_layers = []
 
-        for i, (edge_index, _, size) in enumerate(adjs):
-            x_target = x[:size[1]]
-            x_mean = self.convs_mean[i]((x, x_target), edge_index)
-            x_max = self.convs_max[i]((x, x_target), edge_index)
+        for i in range(self.num_layers):
+            x_mean = self.convs_mean[i](x, edge_index)
+            x_max = self.convs_max[i](x, edge_index)
 
+            x_mean = F.relu(x_mean)
+            x_max = F.relu(x_max)
+
+            # 不再进行全局平均，直接使用节点级别的特征
             all_layers.append(x_mean)
             all_layers.append(x_max)
 
-            if i != self.num_layers - 1:
-                x = F.relu(x)
-
+        # 拼接所有层的输出
         x_final = torch.cat(all_layers, dim=1)
         x_final = self.post_mp(x_final)
         return F.log_softmax(x_final, dim=-1)
 
-# 数据准备
+
+# 加载数据集
+dataset = Planetoid(root='path/to/Planetoid', name='Cora')
+data = dataset[0]
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-dataset = Planetoid(root='data/Planetoid', name='Cora')
-data = dataset[0].to(device)
-
-# 模型初始化
-model = GraphSAGEPlusPlusDAMC(dataset.num_features, 32, dataset.num_classes, num_layers=3).to(device)
-train_loader = NeighborSampler(data.edge_index, node_idx=data.train_mask, sizes=[20, 10, 10], batch_size=64, shuffle=True)
-test_loader = NeighborSampler(data.edge_index, node_idx=data.test_mask, sizes=[20, 10, 10], batch_size=64, shuffle=False)
-# 定义损失函数和优化器
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
-
-# 训练函数
-def train():
-    model.train()
-    total_loss = 0
-    for batch_size, n_id, adjs in train_loader:  # 正确处理从NeighborSampler返回的数据
-        # 将邻居采样信息移动到正确的设备上
-        adjs = [adj.to(device) for adj in adjs]
-        optimizer.zero_grad()
-        # 正确传递邻居采样数据给模型
-        out = model(data.x[n_id].to(device), adjs)
-        # 计算损失
-        loss = criterion(out, data.y[n_id[:batch_size]].to(device))
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    return total_loss / len(train_loader)
-
-
-# 测试函数
-def test():
-    model.eval()
-    correct = 0
-    for batch_size, n_id, adjs in test_loader:  # 正确处理从NeighborSampler返回的数据
-        adjs = [adj.to(device) for adj in adjs]  # 将邻居采样信息移动到正确的设备上
-
-        out = model(data.x[n_id].to(device), adjs)  # 正确传递邻居采样数据给模型
-        pred = out.argmax(dim=1)  # 获取概率最高的类别
-        correct += int(pred.eq(data.y[n_id[:batch_size]].to(device)).sum().item())  # 计算正确预测的数量
-    return correct / int(data.test_mask.sum())  # 返回测试准确率
-
-# 训练和测试过程
-for epoch in range(1, 51):
-    loss = train()
-    print(f'Epoch {epoch}, Loss: {loss:.4f}')
-test_acc = test()
-print(f'Test Accuracy: {test_acc:.4f}')
-
-
-
-'''
-prediction
-# 数据准备和边分割
-data = train_test_split_edges(dataset[0])
-
 data = data.to(device)
+model = GraphSAGEPlusPlusDAMC(dataset.num_features, 32, dataset.num_classes, num_layers=3).to(device)
 
-# 模型初始化
-model = GraphSAGEPlusPlusDAC(dataset.num_features, 32, dataset.num_classes, num_layers=3).to(device)
-
-# 定义损失函数和优化器
-criterion = torch.nn.BCEWithLogitsLoss()
+# 训练过程
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+criterion = torch.nn.CrossEntropyLoss()
 
-# 训练函数
 def train():
     model.train()
-    total_loss = 0
-    for data in train_loader:
-        optimizer.zero_grad()
-        out = model(data.x.to(device), data.train_pos_edge_index.to(device))
-
-        # 正样本损失
-        pos_loss = criterion(out[data.train_pos_edge_index], torch.ones(data.train_pos_edge_index.size(1)).to(device))
-
-        # 负样本损失
-        neg_edge_index = negative_sampling(data.edge_index, num_nodes=data.num_nodes, num_neg_samples=data.train_pos_edge_index.size(1))
-        neg_loss = criterion(out[neg_edge_index], torch.zeros(neg_edge_index.size(1)).to(device))
-
-        loss = pos_loss + neg_loss
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    return total_loss / len(train_loader)
+    optimizer.zero_grad()
+    out = model(data.x.to(device), data.edge_index.to(device))
+    loss = criterion(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+    return loss.item()
 
 # 测试函数
 @torch.no_grad()
 def test():
     model.eval()
-    out = model(data.x.to(device), data.test_pos_edge_index.to(device))
-    # ... (链接预测的评估代码) ...
+    out = model(data.x.to(device), data.edge_index.to(device))
+    pred = out.argmax(dim=1)
+    correct = pred[data.test_mask].eq(data.y[data.test_mask]).sum().item()
+    return correct / int(data.test_mask.sum())
 
 # 训练和测试过程
-for epoch in range(1, 51):
+for epoch in range(1, 201):
     loss = train()
     print(f'Epoch {epoch}, Loss: {loss:.4f}')
+
 test_acc = test()
 print(f'Test Accuracy: {test_acc:.4f}')
-'''
