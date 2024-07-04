a472e2c89783d1e0adbb5ec6237fd47a0086d023
ejwww <51363519+ejwww@users.noreply.github.com>
2024-07-04 16:28:20 +0800

    Update GraphSAGE++Mean.py

--- GraphSAGE++Mean.py
+++ GraphSAGE++Mean.py
@@ -1,31 +1,25 @@
 import torch
 import torch.nn.functional as F
-from matplotlib import pyplot as plt
-from sklearn.manifold import TSNE
-from sklearn.metrics import f1_score
-from torch_geometric.nn import MessagePassing
-from torch_geometric.utils import add_self_loops, degree
-from torch_geometric.datasets import Planetoid, Amazon
-from torch_geometric.nn import SAGEConv
-import torch_geometric.nn as pyg_nn
 import torch.nn as nn
-from torch_geometric.utils import train_test_split_edges
-from torch_geometric.datasets import PPI
+from torch_geometric.nn import SAGEConv
 from torch_geometric.loader import NeighborSampler
-from torch_geometric.utils import negative_sampling
-import time
+from torch_geometric.datasets import Planetoid
 
-class GraphSAGEPlusPlusMean(torch.nn.Module):
+class GraphSAGEPlusPlusDAC(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels_list, out_channels):
         super().__init__()
         self.num_layers = len(hidden_channels_list)
         self.convs_mean = torch.nn.ModuleList()
 
-        for i in range(self.num_layers):
-            in_channels_layer = in_channels if i == 0 else hidden_channels_list[i - 1]
-            self.convs_mean.append(SAGEConv(in_channels_layer, hidden_channels_list[i], aggr='mean'))
+        # 第一层的初始化
+        self.convs_mean.append(SAGEConv(in_channels, hidden_channels_list[0], aggr='mean'))
 
-        self.post_mp = nn.Linear(sum(hidden_channels_list), out_channels)
+        # 其余层的初始化
+        for i in range(1, self.num_layers):
+            self.convs_mean.append(SAGEConv(hidden_channels_list[i-1], hidden_channels_list[i], aggr='mean'))
+
+        # 输出层
+        self.post_mp = nn.Linear(hidden_channels_list[-1], out_channels)
 
     def reset_parameters(self):
         for conv in self.convs_mean:
@@ -33,18 +27,12 @@ class GraphSAGEPlusPlusMean(torch.nn.Module):
         self.post_mp.reset_parameters()
 
     def forward(self, x, adjs):
-        all_layers_output = []
-
         for i, (edge_index, _, size) in enumerate(adjs):
             x_target = x[:size[1]]
             x = self.convs_mean[i]((x, x_target), edge_index)
             if i != self.num_layers - 1:
                 x = F.relu(x)
-
-            all_layers_output.append(x)
-
-        x_final = torch.cat(all_layers_output, dim=1)
-        x_final = self.post_mp(x_final)
+        x_final = self.post_mp(x)
         return F.log_softmax(x_final, dim=-1)
 
 # 数据准备和模型初始化
@@ -52,8 +40,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 dataset = Planetoid(root='data/Planetoid', name='Cora')
 data = dataset[0].to(device)
 
-hidden_dims = [73, 34, 21]  # 定义每层的维度
-model = GraphSAGEPlusPlusMean(dataset.num_features, hidden_dims, dataset.num_classes).to(device)
+# 定义每层的维度，例如 [32, 64, 128]
+hidden_dims = [32, 64, 128]
+model = GraphSAGEPlusPlusDAC(dataset.num_features, hidden_dims, dataset.num_classes).to(device)
+
+# ...[其余代码保持不变]...
+
 
 # 损失函数和优化器
 criterion = torch.nn.CrossEntropyLoss()
@@ -96,55 +88,3 @@ for epoch in range(1, 51):
 test_acc = test()
 print(f'Test Accuracy: {test_acc:.4f}')
 
-
-
-
-'''
-prediction
-# 数据准备和边分割
-data = train_test_split_edges(dataset[0])
-
-data = data.to(device)
-
-# initial
-model = GraphSAGEPlusPlusDAC(dataset.num_features, 32, dataset.num_classes, num_layers=3).to(device)
-
-# optimizer
-criterion = torch.nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
-
-# train
-def train():
-    model.train()
-    total_loss = 0
-    for data in train_loader:
-        optimizer.zero_grad()
-        out = model(data.x.to(device), data.train_pos_edge_index.to(device))
-
-        # positive
-        pos_loss = criterion(out[data.train_pos_edge_index], torch.ones(data.train_pos_edge_index.size(1)).to(device))
-
-        # negtive
-        neg_edge_index = negative_sampling(data.edge_index, num_nodes=data.num_nodes, num_neg_samples=data.train_pos_edge_index.size(1))
-        neg_loss = criterion(out[neg_edge_index], torch.zeros(neg_edge_index.size(1)).to(device))
-
-        loss = pos_loss + neg_loss
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    return total_loss / len(train_loader)
-
-# test
-@torch.no_grad()
-def test():
-    model.eval()
-    out = model(data.x.to(device), data.test_pos_edge_index.to(device))
-    # ... (链接预测的评估代码) ...
-
-# train and test
-for epoch in range(1, 51):
-    loss = train()
-    print(f'Epoch {epoch}, Loss: {loss:.4f}')
-test_acc = test()
-print(f'Test Accuracy: {test_acc:.4f}')
-'''
